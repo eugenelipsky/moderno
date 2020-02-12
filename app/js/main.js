4badad56bd97cb9b4b0c124b9307b61c2c91cdec
eugenelipsky <zvlipskiy@mail.ru>
2020-02-12 08:58:21 +0200

    product slider added, product added

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,16 @@ $(function(){
 
     var mixer = mixitup('.products__inner-box');
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth : "12px",
+        readOnly: true
+    });
+
+    $('.product-slider-inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
 });
