207ee9b4f368ebb439d58af25d2e111929cdde09
eugenelipsky <zvlipskiy@mail.ru>
2020-02-22 23:19:48 +0200

    single product and login popup added

--- app/js/main.js
+++ app/js/main.js
@@ -59,6 +59,15 @@ $(function(){
         $('.header__box').toggleClass('active')
     });
 
+    $('.product-one__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+        $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#'+id).addClass('active-tab').fadeIn();
+        return false;
+    });
+
     var mixer = document.querySelector(".products__inner-box");
     if (mixer) {
         mixer = mixitup('.products__inner-box');
--- gulpfile.js
+++ gulpfile.js
@@ -35,6 +35,7 @@ gulp.task('css', function () {
         'node_modules/magnific-popup/dist/magnific-popup.css',
         'node_modules/rateyo/src/jquery.rateyo.css',
         'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css'
     ])
         .pipe(concat('_libs.scss'))
         .pipe(gulp.dest('app/scss'))
@@ -64,6 +65,7 @@ gulp.task('js', function () {
         'node_modules/mixitup/dist/mixitup.js',
         'node_modules/rateyo/src/jquery.rateyo.js',
         'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js'
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
